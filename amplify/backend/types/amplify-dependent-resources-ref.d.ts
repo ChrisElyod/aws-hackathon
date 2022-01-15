b2da1cea60fb7424db737d3c16e9a2ad2c8828cc
ChrisElyod <sean.chris.doyle@gmail.com>
2022-01-15 10:57:03 -0500

    updated with latest amazon gql changes

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "awshackathonapi": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
